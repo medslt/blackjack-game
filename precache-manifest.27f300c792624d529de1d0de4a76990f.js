cc178e3c0d83066f3b6a234d563c22082d93a000
mohamed salamat <moahmed.salamat@gmail.com>
2020-08-05 07:09:25 +0100

    Updates

---
+++ precache-manifest.27f300c792624d529de1d0de4a76990f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "747e20ac9a565e62237e439848ad7038",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3d11fad81cad59817a3",
+    "url": "/static/css/main.f0670b1e.chunk.css"
+  },
+  {
+    "revision": "c8e281d98aceb9ae0d59",
+    "url": "/static/js/2.8f9be44e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f9be44e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3d11fad81cad59817a3",
+    "url": "/static/js/main.d346101a.chunk.js"
+  },
+  {
+    "revision": "19ca7c68cab22cbeceaf",
+    "url": "/static/js/runtime-main.9812e634.js"
+  }
+]);
